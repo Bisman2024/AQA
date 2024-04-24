30b3655b432d6f2a56d9da71d189500bc8dcd9bc
Bisman2024 <165083447+Bisman2024@users.noreply.github.com>
2024-04-24 23:50:10 +0300

    Практическое задание Л11-12

--- MainTestng.java
+++
@@ -1,9 +0,0 @@
-package org.testng.NewTest;
-
-public class MainTestng {
-    public static void main(String[] args) {
-        int number = 12;
-        long factorial = FactorialTestng.calculateFactorial(number);
-        System.out.println("Факториал числа " + number + " равен " + factorial);
-    }
-}
